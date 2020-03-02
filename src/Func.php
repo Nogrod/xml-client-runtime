146a0a10d571c8e4d93ee8586e6f39ac4e92eec3
Nogrod <strikeomania@gmail.com>
2020-03-02 20:28:29 +0100

    func class

---
+++ src/Func.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Nogrod\XMLClientRuntime;
+
+class Func
+{
+    public static function mapArray(array $array, string $name, bool $isArray = false)
+    {
+        $result = [];
+        foreach ($array as $item) {
+            if ($item['name'] !== $name) {
+                continue;
+            }
+            if ($isArray) {
+                $result[] = $item['value'];
+            } else {
+                return $item['value'];
+            }
+        }
+        return $isArray ? $result : null;
+    }
+}
