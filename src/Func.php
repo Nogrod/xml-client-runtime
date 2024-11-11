ae1152baa4559ad22d37621554bc8b021aec96e4
Nogrod <strikeomania@gmail.com>
2024-11-11 22:05:59 +0100

    func map by type

--- src/Func.php
+++ src/Func.php
@@ -5,21 +5,60 @@ namespace Nogrod\XMLClientRuntime;
 
 class Func
 {
-    public static function mapArray(array $array, string $name, bool $isArray = false)
+    public static function mapArray(array &$array, string $name)
     {
         $result = [];
-        foreach ($array as $item) {
-            if ($item['name'] !== $name) {
+        foreach ($array as $key => $value) {
+            if ($value['name'] !== $name) {
                 continue;
             }
-            if ($isArray) {
-                if ($item['value'] !== null) {
-                    $result[] = $item['value'];
+            $tmpValue = $value['value'];
+            $tmpAttr = $value['attributes'];
+            unset($array[$key]);
+            if ($tmpValue !== null) {
+                foreach ($tmpAttr as $attrKey => $attrValue) {
+                    $tmpValue[] = ['name' => $attrKey, 'value' => $attrValue, 'attributes' => []];
                 }
-            } else {
-                return $item['value'];
+                $result[] = $tmpValue;
             }
         }
-        return $isArray ? $result : null;
+
+        return $result;
+    }
+
+    public static function mapObject(array &$array, string $name)
+    {
+        foreach ($array as $key => $value) {
+            if ($value['name'] !== $name) {
+                continue;
+            }
+            if (is_array($value['value'])) {
+                $tmpValue = $value['value'];
+            } else {
+                $tmpValue = [['name' => 'value', 'value' => $value['value'], 'attributes' => []]];
+            }
+            foreach ($value['attributes'] as $attrKey => $attrValue) {
+                $tmpValue[] = ['name' => $attrKey, 'value' => $attrValue, 'attributes' => []];
+            }
+            unset($array[$key]);
+
+            return $tmpValue;
+        }
+
+        return null;
+    }
+
+    public static function mapValue(array &$array, string $name)
+    {
+        foreach ($array as $key => $value) {
+            if ($value['name'] !== $name) {
+                continue;
+            }
+            $tmpValue = $value['value'];
+            unset($array[$key]);
+            return $tmpValue;
+        }
+
+        return null;
     }
 }
